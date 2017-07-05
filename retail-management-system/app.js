b0fd0687420d91b5f577153f2782a9d2f052f1b5
LaKeisha Folson <lakeisha.folson@ge.com>
2017-07-05 11:00:40 -0400

    update to master

---
+++ retail-management-system/app.js
@@ -0,0 +1,28 @@
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');
+var bodyParser = require('body-parser');
+var app        = express();
+
+// configure body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use(express.static('bower-components'))
+app.use(express.static('public'))		//
+app.use(function(req,res,next){
+	console.log('something happened');
+	next();
+})
+
+var port     = process.env.PORT || 8080; // set our port
+app.get('/', function(req, res){
+	res.json({msg:'test'});
+	res.sendFile('./public/index.html')
+});
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
