06ad5ca5183673c82fb5e3e13da1577bf39e8dbf
LaKeisha Folson <lakeisha.folson@ge.com>
2017-07-17 10:43:37 -0400

    set email to unique

--- schema_models/users.js
+++ schema_models/users.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('user',{
 	name :{type: String, required: true},
 	password: {type: String, required: true},
-	email : {type: String, required: true},
+	email : {type: String, unique: true, required: true},
 	role : {type: String, default: 'user'}
 })
