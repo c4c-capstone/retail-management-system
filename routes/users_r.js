fe9759fb755f1f17628240809ac681a460d7acfd
LaKeisha Folson <lakeisha.folson@ge.com>
2017-07-18 16:09:44 -0400

    No duplicate emails

--- routes/users_r.js
+++ routes/users_r.js
@@ -4,21 +4,30 @@ Customer = require('../schema_models/customer.js'),
 Address = require('../schema_models/address.js');
 
 router.post('/',function(req,res){
-	var customer = req.body.customer;
-	new Address(customer.address)
-		.save()
-		.then(function(address){
-			customer.address = address;
-			return new Customer(customer).save();
-		})
-		.then(function(customer){
-			res.json(customer);
-		})
-		.catch(function(err){
+
+	Customer.findOne({email:req.body.customer.email}, function(err, customer){
+		if(err)
 			console.log(err);
-		});
-	
-})
+		if(customer)
+			res.json({error:'email exists'});
+		if(!err && !customer){
+			customer = req.body.customer;
+		new Address(customer.address)
+			.save()
+			.then(function(address){
+				customer.address = address;
+				return new Customer(customer).save();
+			})
+			.then(function(customer){
+				res.json(customer);
+			})
+			.catch(function(err){
+				console.log(err);
+			});
+
+	}
+  });
+});
 
 router.get('/',function(req,res){
 	Customer
@@ -26,34 +35,34 @@ router.get('/',function(req,res){
 		.populate('address')
 		.exec()
 		.then(function(cust){
-			res.json(cust)
+			res.json(cust);
 		})
 		.catch(function(err){
-			console.log(err)
+			console.log(err);
 		});
-})
+});
 
 router.post('/login',function(req,res){
-	console.log(req.body)
+	console.log(req.body);
 	Customer
 		.findOne({email: req.body.email})
-		//.populate('address')
+		.populate('address')
 		.exec()
 		.then(function(cust){
-			console.log(cust)
-			if(cust.password === req.body.cust.password){
+			console.log(cust);
+			if(cust.password === req.body.password){
 				res.json(cust);
 			}
 			else{
 				console.log('Seriously');
-				res.sendStatus(403)
+				res.sendStatus(403);
 			}
 		})
 		.catch(function(err){
-			console.log(err)
-			console.log('Seriously Bro')
-			res.sendStatus(403)
+			console.log(err);
+			console.log('Seriously Bro');
+			res.sendStatus(403);
 		});
-})
+});
 
 module.exports = router;
