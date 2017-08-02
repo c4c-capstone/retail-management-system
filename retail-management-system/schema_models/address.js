2bf551c0e9b63dfe69fdb48cf87cbc3dc2660b5e
James Thiede <james.thiede@ge.com>
2017-08-02 11:57:52 -0400

    Removed duplicated directory

--- retail-management-system/schema_models/address.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-module.exports = mongoose.model('address',{
-	street: {type: String, required: true},
-	apt: {type: String, required: false},
-	city: {type: String, required: true},
-	state: {type: String, required: true},
-	zip: {type: String, required: true}
-})
